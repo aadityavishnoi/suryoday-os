9200ded6bc50abde7327ddb328368bfb799a8b93
aadityavishnoi <vishnoiaaditya29@gmail.com>
2025-12-30 12:36:32 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,74 +46,81 @@ function App() {
       )}
 
       {/* GOVERNMENT LEADERSHIP */}
-      <div className="bg-white p-4 sm:p-6 border border-gray-200 rounded-md mt-4">
+      <section className="bg-white p-4 sm:p-6 border border-gray-200 rounded-md mt-4">
         <h3 className="text-lg font-semibold text-gray-800 mb-4 text-center">
           सरकारी नेतृत्व
         </h3>
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-
-          <div className="border border-gray-300 rounded-md overflow-hidden bg-gray-50 w-full max-w-[180px] mx-auto">
-            <img src="https://www.presidentofindia.gov.in/sites/default/files/2025-04/president_of_india.jpg" className="w-full h-52 object-cover" />
-            <div className="p-2 text-center">
-              <p className="font-semibold text-sm">श्रीमती द्रौपदी मुर्मू</p>
-              <p className="text-xs text-gray-600">राष्ट्रपति</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {[
+            {
+              name: "श्रीमती द्रौपदी मुर्मू",
+              title: "भारत के माननीय राष्ट्रपति",
+              img: "https://www.presidentofindia.gov.in/sites/default/files/2025-04/president_of_india.jpg"
+            },
+            {
+              name: "श्री नरेंद्र मोदी",
+              title: "भारत के माननीय प्रधानमंत्री",
+              img: "https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/459337720_1303104714517091_2841079786125894817_n.jpg"
+            },
+            {
+              name: "श्री शिवराज सिंह चौहान",
+              title: "कृषि एवं किसान कल्याण मंत्री",
+              img: "https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/367400249_831809051843175_3472940923652535254_n.jpg"
+            },
+            {
+              name: "श्री अमित शाह",
+              title: "भारत के गृह मंत्री",
+              img: "https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/378983958_929288798552749_3157032759071580316_n.jpg"
+            }
+          ].map((p, i) => (
+            <div
+              key={i}
+              className="border border-gray-300 rounded-md bg-gray-50 w-full max-w-[220px] mx-auto overflow-hidden"
+            >
+              <img src={p.img} alt={p.name} className="w-full h-52 object-cover" />
+              <div className="p-3 text-center">
+                <p className="font-semibold text-gray-900">{p.name}</p>
+                <p className="text-sm text-gray-600">{p.title}</p>
+              </div>
             </div>
-          </div>
-
-          <div className="border border-gray-300 rounded-md overflow-hidden bg-gray-50 w-full max-w-[180px] mx-auto">
-            <img src="https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/459337720_1303104714517091_2841079786125894817_n.jpg" className="w-full h-52 object-cover" />
-            <div className="p-2 text-center">
-              <p className="font-semibold text-sm">श्री नरेंद्र मोदी</p>
-              <p className="text-xs text-gray-600">प्रधानमंत्री</p>
-            </div>
-          </div>
-
-          <div className="border border-gray-300 rounded-md overflow-hidden bg-gray-50 w-full max-w-[180px] mx-auto">
-            <img src="https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/367400249_831809051843175_3472940923652535254_n.jpg" className="w-full h-52 object-cover" />
-            <div className="p-2 text-center">
-              <p className="font-semibold text-sm">श्री शिवराज सिंह चौहान</p>
-              <p className="text-xs text-gray-600">कृषि मंत्री</p>
-            </div>
-          </div>
-
-          <div className="border border-gray-300 rounded-md overflow-hidden bg-gray-50 w-full max-w-[180px] mx-auto">
-            <img src="https://scontent.fagr1-3.fna.fbcdn.net/v/t39.30808-6/378983958_929288798552749_3157032759071580316_n.jpg" className="w-full h-52 object-cover" />
-            <div className="p-2 text-center">
-              <p className="font-semibold text-sm">श्री अमित शाह</p>
-              <p className="text-xs text-gray-600">गृह मंत्री</p>
-            </div>
-          </div>
-
+          ))}
         </div>
-      </div>
+      </section>
 
       {/* MENU */}
-      <div className="bg-white p-4 sm:p-6 border border-gray-200 rounded-md mt-6">
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-
+      <section className="bg-white p-4 sm:p-6 border border-gray-200 rounded-md mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
           {[
-            "मंडी भाव","फसल उत्पादन","मृदा कार्ड","निजी उपकरण","जल प्रबंधन",
-            "मौसम / आपदा","विशेषज्ञ चैट","किराया केंद्र","फसल डॉक्टर",
-            "मुनाफा खाता","योजनाएं","ई-मंडी"
+            "मंडी भाव",
+            "फसल उत्पादन",
+            "मृदा कार्ड",
+            "निजी उपकरण",
+            "जल प्रबंधन",
+            "मौसम / आपदा",
+            "विशेषज्ञ चैट",
+            "किराया केंद्र",
+            "फसल डॉक्टर",
+            "मुनाफा खाता",
+            "योजनाएं",
+            "ई-मंडी"
           ].map((title, i) => (
             <div
               key={i}
-              className="border border-gray-300 rounded-md overflow-hidden bg-blue-900 w-full max-w-[180px] mx-auto cursor-pointer"
+              className="border border-gray-300 rounded-md bg-blue-900 w-full max-w-[220px] mx-auto overflow-hidden cursor-pointer"
             >
               <img
                 src="https://www.ecofy.co.in/sites/default/files/inline-images/stree_sakti-image.png"
                 alt={title}
                 className="w-full h-52 object-cover"
               />
-              <div className="p-2 text-center">
-                <p className="font-semibold text-sm text-white">{title}</p>
+              <div className="p-3 text-center">
+                <p className="font-semibold text-white">{title}</p>
               </div>
             </div>
           ))}
-
         </div>
-      </div>
+      </section>
 
     </div>
   );
